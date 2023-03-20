9e5687e76c53c6485657c3263e12a6582482a82a
malek12020 <malekjoseph2020@gmail.com>
2023-03-20 13:17:28 +0200

    fixed dB connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8002;
 const mysql = require('mysql');
 
 
@@ -11,25 +11,43 @@ const mysql = require('mysql');
 //   database: 'scandi-products',
 // });
 
+// const db = mysql.createConnection({
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "password",
+//   database: "CRUDDatabase",
+//   Port: 3306
+// });
 var db = mysql.createConnection({
 
   host: "127.0.0.1",
   user: "root",
-  password: "13101995",
+  password: "password",
   database: "scandi-products",
+  Port: 3306
 });
 
-const sqlInsert = "INSERT INTO products (name, price) VALUES ('SQL COURSE', 15)"
 
 app.get('/', (req, res) => {
 
-  db.query(sqlInsert, (err, result) => {
-    // if (err) throw err;
-    console.log(err);
+
+
+  db.connect((err) => {
+    if (err) throw err;
+    console.log("Connected");
+// const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'good movie')"
+const sqlInsert = "INSERT INTO products (name, price) VALUES ('SQL COURSE', 15)"
+
+    db.query(sqlInsert, function (err, result) {  
+if (err) throw err;  
+console.log("1 record inserted");  
+});  
     res.send('DB working')
   })
   // res.send('Hello World!');
 });
+
+
 
 
 
